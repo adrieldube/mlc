2f76339ef973ef4ecc3da5dbb2d1d106ffd4175a
Adriel Dubé <adrieldubemarante@gmail.com>
2023-01-20 20:07:40 -0600

    Updated script to load external images
    Added new PNG icons

--- src/mlc.js
+++ src/mlc.js
@@ -12,7 +12,26 @@ Changelog:
 const widgetUrl = 'https://eltoque.com/tasas-de-cambio-de-moneda-en-cuba-hoy';
 const apiUrl = `https://api.cambiocuba.money/api/v1/x-rates-by-date-range?trmi=true&period=1D`;
 const currencies = ['ECU', 'USD', 'MLC'];
+const images = [
+    {
+        name: 'eur',
+        url: 'https://adrieldube.github.io/mlc/img/eur.png'
+    },
+    {
+        name: 'usd',
+        url: 'https://adrieldube.github.io/mlc/img/usd.png'
+    },
+    {
+        name: 'mlc',
+        url: 'https://adrieldube.github.io/mlc/img/mlc.png'
+    },
+    {
+        name: 'el-toque',
+        url: 'https://adrieldube.github.io/mlc/img/el-toque.png'
+    }];
+
 const data = await getRates();
+const resources = await getResources();
 
 let widget = createWidget(data, widgetUrl);
 if (config.runsInWidget) {
@@ -29,14 +48,23 @@ function createWidget(data, widgetUrl) {
     const { USD, ECU, MLC } = data;
 
     let w = new ListWidget();
-    w.backgroundColor = new Color('#5200ffe6');
+    w.backgroundColor = new Color('#FFCC00');
+    w.setPadding(0, 0, 0, 0);
+    w.url = widgetUrl;
 
-    let staticText = w.addText('Mercado Informal de Divisas en Cuba');
-    staticText.textColor = new Color('#444');
-    staticText.font = Font.semiboldSystemFont(16);
+    // header
+
+    let staticText = w.addText('Informal Currency Exchange in Cuba');
+    staticText.textColor = new Color('#003399');
+    staticText.font = Font.boldSystemFont(16);
     staticText.centerAlignText();
-
     w.addSpacer(2);
+
+    // body
+
+    let image = w.addImage(getImage('usd'));
+    image.imageSize = new Size(33, 33);
+    image.centerAlignImage();
 
     let eurText = w.addText(`1 EUR - ${format(ECU)} CUP`);
     eurText.textColor = Color.white();
@@ -59,19 +87,21 @@ function createWidget(data, widgetUrl) {
 
     w.addSpacer(2);
 
-    w.setPadding(0, 0, 0, 0);
-    w.url = widgetUrl;
+    // footer
+
+    const date = new Date();
+    const df = new DateFormatter();
+    df.useFullDateStyle();
+
+    const hf = new DateFormatter();
+    hf.useShortTimeStyle();
+
+    const dateText = w.addText(`${df.string(date)} ${hf.string(date)}`);
+    dateText.textColor = new Color('#003399');
+    dateText.font = Font.semiboldSystemFont(10);
+    dateText.centerAlignText();
 
     return w;
-}
-
-async function getRates() {
-    const rates = {};
-    for (const currency of currencies) {
-        const r = await getRate(currency);
-        rates[currency] = r;
-    }
-    return rates;
 }
 
 async function getRate(currency) {
@@ -84,6 +114,34 @@ async function getRate(currency) {
     return avg;
 }
 
+async function getRates() {
+    const rates = {};
+    for (const currency of currencies) {
+        const r = await getRate(currency);
+        rates[currency] = r;
+    }
+    return rates;
+}
+
+async function getResources() {
+    const result = {};
+
+    for (const r of images) {
+        const { name, url } = r;
+        console.log(name, url);
+        const req = new Request(url);
+        const res = await req.loadImage();
+        result[name] = res;
+    }
+
+    return result;
+}
+
+function getImage(id) {
+    return resources[id];
+}
+
 function format(v) {
     return Math.round(Number.parseFloat(v)).toFixed(2);
 }
+
